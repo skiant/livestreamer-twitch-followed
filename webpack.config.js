c2979d1ccdb30f77b17c56deab9c56eb6e6187a0
Mathieu Dubois <skiant.be@gmail.com>
2016-04-10 15:42:01 +0200

    Preparing the deployment to NPM

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: './bin/',
+		filename: 'cli.js',
+	},
+	module: {
+		preLoaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader' },
+		],
+		loaders: [
+			{ test: /\.js$/, loader: 'babel' },
+		],
+	},
+	plugins: [
+		new webpack.BannerPlugin('#!/usr/bin/env node'),
+	],
+	node: {
+		fs: 'empty',
+		child_process: 'empty',
+		readline: 'empty',
+	},
+};
